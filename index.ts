e0d42604a11385f21c4337e34ed45c927e5e9759
Zoltán Zemko <zemko_z@hotmail.com>
2023-04-23 16:31:09 +0200

    3.4 - Extract code from update()function to handleInputs() and updateMap().

--- index.ts
+++ index.ts
@@ -81,6 +81,11 @@ function moveVertical(dy: number) {
 }
 
 function update() {
+  handleInputs();
+  updateMap();
+}
+
+function handleInputs() {
   while (inputs.length > 0) {
     let current = inputs.pop();
     if (current === Input.LEFT)
@@ -92,15 +97,17 @@ function update() {
     else if (current === Input.DOWN)
       moveVertical(1);
   }
+}
 
+function updateMap() {
   for (let y = map.length - 1; y >= 0; y--) {
     for (let x = 0; x < map[y].length; x++) {
       if ((map[y][x] === Tile.STONE || map[y][x] === Tile.FALLING_STONE)
-        && map[y + 1][x] === Tile.AIR) {
+          && map[y + 1][x] === Tile.AIR) {
         map[y + 1][x] = Tile.FALLING_STONE;
         map[y][x] = Tile.AIR;
       } else if ((map[y][x] === Tile.BOX || map[y][x] === Tile.FALLING_BOX)
-        && map[y + 1][x] === Tile.AIR) {
+          && map[y + 1][x] === Tile.AIR) {
         map[y + 1][x] = Tile.FALLING_BOX;
         map[y][x] = Tile.AIR;
       } else if (map[y][x] === Tile.FALLING_STONE) {
@@ -113,13 +120,16 @@ function update() {
 }
 
 function draw() {
-  let canvas = document.getElementById("GameCanvas") as HTMLCanvasElement;
-  let g = canvas.getContext("2d");
-
-  g.clearRect(0, 0, canvas.width, canvas.height);
-
+  let g = createGraphics();
   drawMap(g);
   drawPlayer(g);
+}
+
+function createGraphics() {
+  let canvas = document.getElementById("GameCanvas") as HTMLCanvasElement;
+  let g = canvas.getContext("2d");
+  g.clearRect(0, 0, canvas.width, canvas.height);
+  return g;
 }
 
 function drawMap(g: CanvasRenderingContext2D) {
